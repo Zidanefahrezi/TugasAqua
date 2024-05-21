edabeeb4e1ee0618e4e649cf11c8926ec3ab207b
Zidanefahrezi <aezidan1@gmail.com>
2024-05-21 20:58:45 +0700

    TugasBesar1_Updatess

--- app/src/main/java/com/example/tugasaqua/Mainscan_tutupbotol.java
+++ app/src/main/java/com/example/tugasaqua/Mainscan_tutupbotol.java
@@ -1,25 +1,33 @@
 package com.example.tugasaqua;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.budiyev.android.codescanner.AutoFocusMode;
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.budiyev.android.codescanner.ErrorCallback;
 import com.budiyev.android.codescanner.ScanMode;
+import com.google.zxing.Result;
 
 public class Mainscan_tutupbotol extends AppCompatActivity {
 
     private static final int CAMERA_REQUEST_CODE = 101;
+
     private CodeScanner codeScanner;
     private CodeScannerView scannerView;
     private TextView tv_textView;
@@ -29,16 +37,25 @@ public class Mainscan_tutupbotol extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scan_tutup_botol);
 
+        ImageView imageView = findViewById(R.id.img_textView);
+        imageView.setVisibility(View.INVISIBLE);
+
         Button menu = findViewById(R.id.vector);
-        menu.setOnClickListener(view -> {
-            Intent intent = new Intent(Mainscan_tutupbotol.this, MainActivity.class);
-            startActivity(intent);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Mainscan_tutupbotol.this, MainBerandaUtama.class);
+                startActivity(intent);
+            }
         });
 
         Button btnlogin = findViewById(R.id.btnlogin);
-        btnlogin.setOnClickListener(view -> {
-            Intent intent = new Intent(Mainscan_tutupbotol.this, com.example.tugasaqua.Mainscan_KTP.class);
-            startActivity(intent);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Mainscan_tutupbotol.this, Mainscan_KTP.class);
+                startActivity(intent);
+            }
         });
 
         tv_textView = findViewById(R.id.tv_textView);
@@ -49,6 +66,7 @@ public class Mainscan_tutupbotol extends AppCompatActivity {
     }
 
     private void codeScanner() {
+        final ImageView imageView = findViewById(R.id.img_textView);
         codeScanner = new CodeScanner(this, scannerView);
 
         codeScanner.setCamera(CodeScanner.CAMERA_BACK);
@@ -58,11 +76,38 @@ public class Mainscan_tutupbotol extends AppCompatActivity {
         codeScanner.setAutoFocusEnabled(true);
         codeScanner.setFlashEnabled(true);
 
-        codeScanner.setDecodeCallback(result -> runOnUiThread(() -> tv_textView.setText(result.getText())));
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        imageView.setVisibility(View.VISIBLE);
+                        // tv_textView.setText(result.getText()); // Uncomment this line if you want to display the scanned text
+                    }
+                });
+            }
+        });
 
-        codeScanner.setErrorCallback(error -> runOnUiThread(() -> Log.e("main", "camera error, " + error.getMessage())));
+        codeScanner.setErrorCallback(new ErrorCallback() {
+            @Override
+            public void onError(@NonNull final Throwable error) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.e("main", "camera error, " + error.getMessage());
+                    }
+                });
+            }
+        });
 
-        scannerView.setOnClickListener(view -> codeScanner.startPreview());
+
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                codeScanner.startPreview();
+            }
+        });
     }
 
     @Override
@@ -74,10 +119,11 @@ public class Mainscan_tutupbotol extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        codeScanner.releaseResources();
     }
 
     private void setupPermission() {
-        int permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA);
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
 
         if (permission != PackageManager.PERMISSION_GRANTED) {
             makeRequest();
@@ -85,11 +131,11 @@ public class Mainscan_tutupbotol extends AppCompatActivity {
     }
 
     private void makeRequest() {
-        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == CAMERA_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
